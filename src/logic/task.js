711ec07d69dbde6fcfcb9297a8ef6ede4d1a49de
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-12-09 17:39:17 +0100

    Create Task constructor function

---
+++ src/logic/task.js
@@ -0,0 +1,6 @@
+export default function createTask(title, dueDate, priority, checkList) {
+  const title = title;
+  const dueDate = dueDate;
+  const priority = priority;
+  const checkList = priority;
+}
